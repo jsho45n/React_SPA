9dceb0db03f13393e0a8a99c2bfc46010ed4e003
jsho45n <tpgns0223@dsm.hs.kr>
2019-11-21 15:32:00 +0900

    리액트 라우터 공부 파일

---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import WithRouterSample from './WithRouterSample';
+
+const data = {
+    velopert: {
+        name: '김민준',
+        description: '리액트를 좋아하는 개발자'
+    },
+    sehoon: {
+        name: '전세훈',
+        description: '리액트를 공부하는 학생'
+    }
+};
+
+const Profile = ({ match }) => {
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) {
+        return <div>존재하지 않는 사용자입니다.</div>
+    }
+    return (
+        <div>
+            <h3>{username}({profile.name})</h3>
+            <p>{profile.description}</p>
+            <WithRouterSample />
+        </div>
+    )
+};
+
+export default Profile;
